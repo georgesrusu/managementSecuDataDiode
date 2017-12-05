ba6656d0b2b636062aec229db835f33a88c858e1
George Rusu <georgesrusu@yahoo.fr>
2017-12-05 15:18:02 +0100

    rien de nouveau

--- DataDiodeReceiver/receiver/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ReceiverConfig(AppConfig):
-    name = 'receiver'
--- DataDiodeTransmitter/launchDjango.sh
+++
@@ -1,5 +0,0 @@
-IP=`cat /etc/network/interfaces | grep address |awk '{print $2;}'| grep 192`
-echo $IP
-
-cd /home/transmitter/Desktop/DataDiodeTransmitter/
-python3 manage.py runserver $IP:8080
