65e8bc3d207837580c7295d5710c4ec45b0025df
George Rusu <georgesrusu@yahoo.fr>
2017-12-07 22:09:25 +0100

    rapport fini coté George, ajoute dossier pour demonstration

--- DataDiodeReceiver/Login/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from Login import views
-
-urlpatterns = [
-    url(r'^$', views.loginReceiver, name='login'),  #---------receiver---------
-    url(r'^logout/$', views.logoutUser, name='logout'),
-]
--- DataDiodeReceiver/launchDjango.sh
+++
@@ -1,5 +0,0 @@
-IP=`cat /etc/network/interfaces | grep address |awk '{print $2;}'| grep 192`
-echo $IP
-
-cd /home/receiver/Desktop/DataDiodeReceiver/
-python3 manage.py runserver $IP:8080
